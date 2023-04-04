4dde10379a160367182453222698690863f684b1
Amr Yasin <61648679+Amryasin@users.noreply.github.com>
2023-04-04 12:46:43 +0200

    first commit

---
+++ lib/src/widgets/apple_pay.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:moyasar/src/utils/apple_pay_utils.dart';
+
+import 'package:pay/pay.dart';
+
+import 'package:moyasar/moyasar.dart';
+import 'package:moyasar/src/moyasar.dart';
+import 'package:moyasar/src/models/payment_request.dart';
+import 'package:moyasar/src/models/sources/apple_pay/apple_pay_request_source.dart';
+
+/// The widget that shows the Apple Pay button.
+class ApplePay extends StatefulWidget {
+  const ApplePay(
+      {super.key, required this.config, required this.onPaymentResult});
+
+  final PaymentConfig config;
+  final Function onPaymentResult;
+
+  @override
+  State<ApplePay> createState() => _ApplePayState();
+}
+
+class _ApplePayState extends State<ApplePay> {
+  String _merchantName = "Bolver";
+
+  @override
+  initState() {
+    super.initState();
+    setMerchantName();
+  }
+
+  void setMerchantName() async {
+    String merchantName = await ApplePayUtils.getMerchantName();
+    setState(() {
+      _merchantName = merchantName;
+    });
+  }
+
+  void onApplePayError(error) {
+    widget.onPaymentResult(PaymentCanceledError());
+  }
+
+  void onApplePayResult(paymentResult) async {
+    final token = paymentResult['token'];
+    final source = ApplePayPaymentRequestSource(token);
+    final paymentRequest = PaymentRequest(widget.config, source);
+
+    final result = await Moyasar.pay(
+        apiKey: widget.config.publishableApiKey,
+        paymentRequest: paymentRequest);
+
+    widget.onPaymentResult(result);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_merchantName != '') {
+      return ApplePayButton(
+        paymentItems: [
+          PaymentItem(
+            label: _merchantName,
+            amount: (widget.config.amount / 100).toStringAsFixed(2),
+          )
+        ],
+        type: ApplePayButtonType.inStore,
+        onPaymentResult: onApplePayResult,
+        width: MediaQuery.of(context).size.width,
+        height: 40,
+        onError: onApplePayError,
+        loadingIndicator: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+}
